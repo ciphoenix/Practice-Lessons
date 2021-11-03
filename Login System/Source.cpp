37e2ffa81412f286ea58364782fe4229f2029673
ciphoenix <ciphoenix@pm.me>
2021-11-03 00:03:22 +0000

    Added file handling methods

--- Login System/Source.cpp
+++ Login System/Source.cpp
@@ -1,50 +1,118 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <filesystem>
 
 using std::string;
 using std::cout;
 using std::cin;
 using std::vector;
 using std::endl;
+using std::ofstream;
+using std::ifstream;
+using std::getline;
+
 
 int serialNumber = 1;
 int recordIndex;
 
-vector<string> usernames = { "defaultName" };
-vector<string> passwords = { "defaultPass" };
+vector<string> usernames = {};
+vector<string> passwords = {};
 
 bool isLoggedIn(string un, string pw);
 void createAccount();
 void loginAccount();
-
+void begin();
+bool readFiles();
+void createFile();
+void writeToFile();
 
 
 int main()
+{
+	createFile();
+
+	readFiles();
+	
+	begin();
+
+	writeToFile();
+
+	return 0;
+
+}
+
+
+void createFile()
+{
+	ofstream MyUsernames("usernames.txt");
+	ofstream MyPasswords("passwords.txt");
+}
+
+void writeToFile()
+{
+	string username, password;
+	ofstream MyUsernames("usernames.txt");
+	ofstream MyPasswords("passwords.txt");
+	MyUsernames.open("usernames.txt");
+	MyPasswords.open("passwords.txt");
+	for (int i = 0; i < usernames.size(); i++)
+	{
+		MyUsernames << usernames[i] << endl;
+	}
+	for (int i = 0; i < passwords.size(); i++)
+	{
+		MyPasswords << passwords[i] << endl;
+	}
+	MyUsernames.close();
+	MyPasswords.close();
+
+	
+}
+
+bool readFiles()
+{
+	string username, password;
+	ifstream MyUsernames("usernames.txt");
+	ifstream MyPasswords("passwords.txt");
+
+	while (getline(MyUsernames, username))
+	{
+		usernames.push_back(username);
+	}
+	while (getline(MyPasswords, password))
+	{
+		passwords.push_back(password);
+	}
+	MyUsernames.close();
+	MyPasswords.close();
+	return true;
+}
+
+void begin()
 {
 	int choice;
 	cout << "1: Set up an account" << endl << "2: Sign in to your account" << endl << "what would you like to do: ";
 	cin >> choice;
 	switch (choice)
 	{
-		case 1:
-			createAccount();
-			system("cls");
-			break;
-		case 2:
-			loginAccount();
-			system("cls");
-			break;
-		default:
-			cout << "invalid entry";
+	case 1:
+		createAccount();
+		system("cls");
+		break;
+	case 2:
+		loginAccount();
+		system("cls");
+		break;
+	default:
+		cout << "invalid entry";
 	}
-
-	return 0;
-
 }
 
 bool isLoggedIn(string un, string pw)
 {
+	recordIndex = recordIndex - 1;
 	if (un == usernames[recordIndex] && pw == passwords[recordIndex])
 	{
 		return true;
@@ -76,7 +144,7 @@ void createAccount()
 	}
 	else {
 		system("cls");
-		loginAccount();
+		begin();
 	}
 }
 
@@ -98,4 +166,9 @@ void loginAccount()
 	}
 	else
 		cout << "Wrong username or password";
+	string prompt;
+	cout << "Type 'Exit' to log out: ";
+	cin >> prompt;
+	if (prompt == "Exit")
+		begin();
 }
