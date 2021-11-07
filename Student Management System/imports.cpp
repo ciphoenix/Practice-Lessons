9591159b303ddfd5e8f224e72a5af91b59a55bed
Joshua Okolie <ciphoenix@users.noreply.github.com>
2021-11-07 21:25:13 +0000

    Add files via upload

---
+++ Student Management System/imports.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include "Patient.h"
+#include <vector>
+#include <fstream>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::vector;
+using std::endl;
+using std::getline;
+using std::fstream;
+using std::ofstream;
+using std::ifstream;
